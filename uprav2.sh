63e03713408ff4e359fee97f34416ba51dad0210
Adam Policky <adam.policky@gmail.com>
2019-04-29 21:26:56 +0200

    Dokumentace

--- uprav2.sh
+++
@@ -1,8 +0,0 @@
-mkdir vstupy2_upr
-
-for i in vstupy/vstup2-*
-do
-	jmeno=`echo $i | cut -f2 -d'/'`
-	cat $i | sed 's/^\( \)*//' | sed 's/ -->\( \)*/ /' | sed 's/\( \)*(\( \)*/ /' | sed 's/\( \)*)//' > vstupy2_upr/$jmeno
-
-done
